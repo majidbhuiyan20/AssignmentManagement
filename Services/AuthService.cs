6d3745577104d8ffde7c206d8fa323709a6fdaea
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-08-06 21:54:38 +0600

    Implement user registration business logic

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -18,7 +18,28 @@ public class AuthService : IAuthService
 
     public async Task RegisterAsync(RegisterRequest request)
     {
-        // Logic will come here
+       bool emailExists = await _context.Users.AnyAsync(u => u.Email == request.Email);
+       if(emailExists)
+        {
+            throw new Exception("Email already exists");
+        }
+        string passwordHash = _passwordHasher.HashPassword(request.Password);
+
+        if (!Enum.TryParse<UserRole>(request.Role, true, out var role))
+        {
+            throw new Exception("Invalid role.");
+        }
+
+        User user = new User
+        {
+            FullName = request.FullName,
+            Email = request.Email,
+            PasswordHash = passwordHash,
+            Role = role,
+            CreatedAt = DateTime.UtcNow
+        };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
     
     }
 
